02cfc7ba078e16a2120fbdaa6a3f6c687755e22d
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-01-17 21:10:50 +0300

    File System Created
    File system to add, delete and updated created and it is as on disk established

---
+++ eCademiaApp.Core/Utilities/BusinessRules/BusinessRules.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Results;
+
+namespace eCademiaApp.Core.Utilities.BusinessRules
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+                if (!logic.Success)
+                    return logic;
+
+            return null;
+        }
+    }
+}
