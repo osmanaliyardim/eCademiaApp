8fe3151ed3ab2edb0fca7368cb12d5e7f7207b45
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-01-17 21:05:54 +0300

    Transaction Aspect
    Transaction aspect created but not implemented

---
+++ eCademiaApp.Core/Aspects/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,26 @@
+﻿using Castle.DynamicProxy;
+using eCademiaApp.Core.Utilities.Interceptors;
+using System.Transactions;
+
+namespace eCademiaApp.Core.Aspects.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (var transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (Exception e)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
